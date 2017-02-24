b97e9435962b033a0fdc8cc49f6c01312f73aa73
Liqun Peng <liqun.peng@springernature.com>
2017-02-24 15:22:39 +0100

    add sys.float_info and sys.int_info

---
+++ src/org/python/modules/sys/FloatInfo.java
@@ -0,0 +1,53 @@
+package org.python.modules.sys;
+
+import org.python.core.PyObject;
+import org.python.core.PyType;
+import org.python.expose.ExposedGet;
+import org.python.expose.ExposedType;
+
+/**
+ * Created by isaiah on 2/23/17.
+ */
+@ExposedType(name = "sys.float_info")
+public class FloatInfo extends PyObject {
+    public static final PyType TYPE = PyType.fromClass(FloatInfo.class);
+    // sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308, min=2.2250738585072014e-308, min_exp=-1021, min_10_exp=-307, dig=15, mant_dig=53,
+    // epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+    @ExposedGet
+    public double max;
+    @ExposedGet
+    public int max_exp;
+    @ExposedGet
+    public int max_10_exp;
+    @ExposedGet
+    public double min;
+    @ExposedGet
+    public int min_exp;
+    @ExposedGet
+    public int min_10_exp;
+    @ExposedGet
+    public int dig;
+    @ExposedGet
+    public int mant_dig;
+    @ExposedGet
+    public double epsilon;
+    @ExposedGet
+    public int radix;
+    @ExposedGet
+    public int rounds;
+
+    public FloatInfo() {
+        max = 1.7976931348623157e+308;
+        max_exp = 1024;
+        max_10_exp = 308;
+        min = 2.2250738585072014e-308;
+        min_exp = -1021;
+        min_10_exp = -307;
+        dig = 15;
+        mant_dig = 53;
+        epsilon = 2.220446049250313e-16;
+        radix = 2;
+        rounds = 1;
+    }
+}
