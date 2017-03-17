8072419ec3eefd92c100fabf24665e8478154faf
Isaiah Peng <issaria@gmail.com>
2017-03-17 21:52:39 +0100

    Replace TryExcept and TryFinally with Try in AST visitor

--- src/org/python/compiler/CodeCompiler.java
+++ src/org/python/compiler/CodeCompiler.java
@@ -1438,7 +1438,7 @@ public class CodeCompiler extends Visitor implements Opcodes, ClassConstants {
         return null;
     }
 
-    public void exceptionTest(int exc, Label end_of_exceptions, TryExcept node, int index)
+    public void exceptionTest(int exc, Label end_of_exceptions, Try node, int index)
             throws Exception {
         for (int i = 0; i < node.getInternalHandlers().size(); i++) {
             ExceptHandler handler = (ExceptHandler)node.getInternalHandlers().get(i);
@@ -1473,8 +1473,11 @@ public class CodeCompiler extends Visitor implements Opcodes, ClassConstants {
         code.athrow();
     }
 
-    @Override
-    public Object visitTryFinally(TryFinally node) throws Exception {
+//    @Override
+    private Object visitTryExcept(Try node) throws Exception {
+        if (node.getInternalFinalbody() == null) {
+            return visitTryExcept(node);
+        }
         Label start = new Label();
         Label end = new Label();
         Label handlerStart = new Label();
@@ -1529,8 +1532,9 @@ public class CodeCompiler extends Visitor implements Opcodes, ClassConstants {
         code.freeLocal(excLocal);
 
         inFinally.addExceptionHandlers(handlerStart);
+        return visitTryExcept(node);
         // According to any JVM verifiers, this code block might not return
-        return null;
+//        return null;
     }
 
     private void inlineFinally(ExceptionHandler handler) throws Exception {
@@ -1574,7 +1578,7 @@ public class CodeCompiler extends Visitor implements Opcodes, ClassConstants {
     }
 
     @Override
-    public Object visitTryExcept(TryExcept node) throws Exception {
+    public Object visitTry(Try node) throws Exception {
         Label start = new Label();
         Label end = new Label();
         Label handler_start = new Label();
@@ -3460,8 +3464,8 @@ public class CodeCompiler extends Visitor implements Opcodes, ClassConstants {
         }
 
         public void finalBody(CodeCompiler compiler) throws Exception {
-            if (node instanceof TryFinally) {
-                suite(((TryFinally)node).getInternalFinalbody());
+            if (node instanceof Try) {
+                suite(((Try)node).getInternalFinalbody());
             }
         }
     }
