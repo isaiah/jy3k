1fafbea389382c63e207b1984c51d91564bc7329
Isaiah Peng <issaria@gmail.com>
2017-03-26 18:25:20 +0200

    add _sre_compile module as a replacement for sre_compile

--- src/org/python/modules/sre/_sre_compile.java
+++ src/org/python/modules/sre/_sre_compile.java
@@ -1,7 +1,19 @@
 package org.python.modules.sre;
 
+import org.python.core.Py;
+import org.python.core.PyObject;
+import org.python.expose.ExposedFunction;
+import org.python.expose.ExposedModule;
+import org.python.internal.joni.Regex;
+
 /**
  * Created by isaiah on 3/24/17.
  */
+@ExposedModule(name="_sre_compile", doc="Native implementation of sre_compile")
 public class _sre_compile {
+
+    @ExposedFunction
+    public static PyObject compile(String s) {
+        return new PySRE_Pattern(s);
+    }
 }
