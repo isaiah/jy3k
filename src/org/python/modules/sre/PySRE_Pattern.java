4febf3686199b8e749987f03637517dd6c142747
Isaiah Peng <issaria@gmail.com>
2017-03-27 14:49:30 +0200

    add pattern#sub

--- src/org/python/modules/sre/PySRE_Pattern.java
+++ src/org/python/modules/sre/PySRE_Pattern.java
@@ -101,14 +101,53 @@ public class PySRE_Pattern extends PyObject {
             Region region = matcher.getEagerRegion();
             if (region.beg[0] != pos) {
                 list.add(new PyUnicode(s.substring(pos, region.beg[0])));
+                pos = region.end[0];
+            } else {
+                pos++;
             }
 
             for (int i = 1; i < region.beg.length; i++) {
                 list.add(new PyUnicode(s.substring(region.beg[i], region.end[i])));
             }
-            pos = ++result;
         }
         return new PyList(list);
+    }
+
+    @ExposedMethod
+    public PyObject SRE_Pattern_sub(PyObject[] args, String[] keywords) {
+        PyObject filter = args[0];
+        ArgParser ap = new ArgParser("sub", args, keywords, "repl", "string", "count");
+        String s = ap.getString(1);
+        int count = ap.getInt(2, 0);
+        boolean replCallable = filter.isCallable();
+        String replacement = null;
+        if (!replCallable) {
+            replacement = ap.getString(0);
+        }
+        StringBuilder sb = new StringBuilder();
+        Matcher matcher = reg.matcher(s.getBytes());
+        for (int pos = 0; pos < s.length();) {
+            int result = matcher.search(pos, s.length(), Option.DEFAULT);
+            if (result == -1) {
+                sb.append(s.substring(pos));
+                break;
+            }
+
+            Region region = matcher.getEagerRegion();
+            if (region.beg[0] != pos) {
+                sb.append(s.substring(pos, region.beg[0]));
+                pos = region.end[0];
+            } else {
+                pos++;
+            }
+
+            if (replCallable) {
+                PyObject match = new PySRE_Match(matcher, s, this);
+                replacement = ((PyUnicode) filter.__call__(match)).getString();
+            }
+            sb.append(replacement);
+        }
+        return new PyUnicode(sb);
     }
 
     @ExposedGet(name = "groups")
