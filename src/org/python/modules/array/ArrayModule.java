17f49e150a40911cbdbf42762e99f6927876d419
Isaiah Peng <issaria@gmail.com>
2017-11-28 14:18:30 +0100

    use static version of the type object

--- src/org/python/modules/array/ArrayModule.java
+++ src/org/python/modules/array/ArrayModule.java
@@ -1,7 +1,10 @@
 //Copyright (c) Corporation for National Research Initiatives
 package org.python.modules.array;
 
+import org.python.core.BufferProtocol;
 import org.python.core.BuiltinDocs;
+import org.python.core.Py;
+import org.python.core.PyLong;
 import org.python.core.PyObject;
 import org.python.core.PyType;
 import org.python.annotations.ExposedConst;
@@ -20,14 +23,32 @@ public class ArrayModule {
 
     @ModuleInit
     public static void classDictInit(PyObject dict) {
-        dict.__setitem__("array", PyType.fromClass(PyArrayArray.class));
-        dict.__setitem__("ArrayType", PyType.fromClass(PyArrayArray.class));
+        dict.__setitem__("array", PyArrayArray.TYPE);
+        dict.__setitem__("ArrayType", PyArrayArray.TYPE);
     }
 
     @ExposedFunction
-    public static final PyObject _array_reconstructor(PyType arraytype, String typecode, PyObject mformat_code,
+    public static final PyObject _array_reconstructor(PyObject arraytype, String typecode, PyObject mformat_code,
                                                       PyObject items) {
-        PyArrayArray ret = new PyArrayArray(arraytype, MachineFormatCode.formatCode(typecode.charAt(0)), items.__len__());
+        if (arraytype instanceof PyType) {
+            if (!Py.isSubClass(arraytype, PyArrayArray.TYPE)) {
+                throw Py.TypeError(String.format("%s is not a subtype of array.array", arraytype));
+            }
+        } else {
+            throw Py.TypeError(String.format("first argument must be a type object, not %s", arraytype.getType().fastGetName()));
+        }
+        if (mformat_code instanceof PyLong) {
+            int mcode = mformat_code.asInt();
+            if (mcode < 0 || mcode > 21) {
+                throw Py.ValueError("third argument must be a valid machine format code.");
+            }
+        } else {
+            throw Py.TypeError(String.format("an integer is required (got %s)", mformat_code.getType().fastGetName()));
+        }
+        if (!(items instanceof BufferProtocol)) {
+            throw Py.TypeError(String.format("fourth argument should be bytes, not %s", items.getType().fastGetName()));
+        }
+        PyArrayArray ret = new PyArrayArray((PyType) arraytype, MachineFormatCode.formatCode(typecode.charAt(0)), items.__len__());
         ret.extend(items);
         return ret;
     }
