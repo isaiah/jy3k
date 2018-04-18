ac01a14e9556614424b35b07dfe53e80d9056f20
Isaiah Peng <issaria@gmail.com>
2018-04-18 16:42:04 +0200

    independent illustration of the StAX bug

--- org.python/src/org/python/util/Asmcheck.java
+++ org.python/src/org/python/util/Asmcheck.java
@@ -1,15 +1,23 @@
 package org.python.util;
 
-import org.python.core.PyFrame;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.StringReader;
 
 /**
  * Created by isaiah on 3/16/17.
  */
 public class Asmcheck {
-    public static void main(String[] args) {
-        Integer a = 0;
-        Object[] arr = new Object[3];
-        arr[a] = 2;
-        System.out.println(a);
+    public static void main(String[] args) throws XMLStreamException {
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        inputFactory.setXMLReporter((message, errorType, relatedInformation, location) -> {
+            System.out.println(message);
+            System.out.println(errorType);
+        });
+        XMLStreamReader reader = inputFactory.createXMLStreamReader(new StringReader("<a>"));
+        while (reader.hasNext()) {
+            reader.next();
+        }
     }
 }
